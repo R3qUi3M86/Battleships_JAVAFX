223d91cd4775ff253462904f74335975c35746ce
R3qUi3M86 <requiem123@gmail.com>
2022-03-12 19:10:51 +0100

    Implemented main menu and settings menu

--- src/main/java/com/battleships/view/WindowMenuController.java
+++ src/main/java/com/battleships/view/WindowMenuController.java
@@ -1,9 +1,92 @@
 package com.battleships.view;
 
 import com.battleships.controller.GameController;
+import com.battleships.controller.GameMode;
+import com.battleships.controller.ShootingMode;
+import com.battleships.controller.ViewController;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Slider;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
 
 public class WindowMenuController {
-    public void onMainMenuBtnClick(String option){
+
+    @FXML
+    public Button gameModeBtn;
+    public Slider boardSizeSlider;
+    public Button shootingModeBtn;
+    public Slider shotsSlider;
+    public HBox shotAmtGroup;
+    public Button backBtn;
+    public Text brdSizeNumber;
+    public Text shotsAmtNumber;
+
+    public void onMenuBtnClick(String option){
         GameController.getInstance().initSelectedMenuOption(Integer.parseInt(option));
+    }
+
+    private void onGameModeBtnClick(){
+        if (GameController.getInstance().getGameMode() == GameMode.PLAYER_VS_PLAYER) {
+            GameController.getInstance().setGameMode(GameMode.PLAYER_VS_COMPUTER);
+        } else {
+            GameController.getInstance().setGameMode(GameMode.PLAYER_VS_PLAYER);
+        }
+        setGameModeBtn(GameController.getInstance().getGameMode());
+    }
+
+    private void onShootModeBtnClick(){
+        if (GameController.getInstance().getShootingMode() == ShootingMode.DYNAMIC) {
+            GameController.getInstance().setShootingMode(ShootingMode.STATIC);
+        } else {
+            GameController.getInstance().setShootingMode(ShootingMode.DYNAMIC);
+        }
+        setShootModeBtn(GameController.getInstance().getShootingMode());
+    }
+
+    private void onBoardSizeSliderChange(){
+        GameController.getInstance().setBoardSize((int) boardSizeSlider.getValue());
+        setBoardSizeSlider(GameController.getInstance().getBoardSize());
+    }
+
+    private void onShotsSliderChange(){
+        GameController.getInstance().setShotsPerPlayer((int) shotsSlider.getValue());
+        setShotsSlider(GameController.getInstance().getShotsPerPlayer());
+    }
+
+    public void setGameModeBtn(GameMode mode){
+        if (mode == GameMode.PLAYER_VS_PLAYER){
+            gameModeBtn.setText("Player vs Player");
+        } else {
+            gameModeBtn.setText("Player vs Computer");
+        }
+    }
+
+    public void setShootModeBtn(ShootingMode mode){
+        if (mode == ShootingMode.DYNAMIC){
+            shootingModeBtn.setText("Dynamic");
+            shotAmtGroup.setStyle("visibility: hidden");
+        } else {
+            shootingModeBtn.setText("Static");
+            shotAmtGroup.setStyle("visibility: visible");
+        }
+    }
+
+    public void setBoardSizeSlider(int boardSize){
+        boardSizeSlider.setValue(boardSize);
+        brdSizeNumber.setText(String.valueOf(boardSize));
+    }
+
+    public void setShotsSlider(int shootsAmt){
+        shotsSlider.setValue(shootsAmt);
+        shotsAmtNumber.setText(String.valueOf(shootsAmt));
+    }
+
+    public void addActionListeners(){
+        gameModeBtn.setOnAction(e -> onGameModeBtnClick());
+        shootingModeBtn.setOnAction(e -> onShootModeBtnClick());
+        boardSizeSlider.setOnMouseReleased(e -> onBoardSizeSliderChange());
+        shotsSlider.setOnMouseReleased(e -> onShotsSliderChange());
+        backBtn.setOnAction(e -> ViewController.getInstance().displayMainMenu());
     }
 }
--- src/main/java/com/battleships/view/WindowMenuRenderer.java
+++ src/main/java/com/battleships/view/WindowMenuRenderer.java
@@ -3,6 +3,7 @@ package com.battleships.view;
 import com.battleships.BattleshipsWindowed;
 import com.battleships.controller.GameMode;
 import com.battleships.controller.ShootingMode;
+import javafx.scene.Cursor;
 import javafx.scene.image.Image ;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -16,36 +17,50 @@ import java.util.ArrayList;
 
 public class WindowMenuRenderer {
     Scene menuScene;
-    WindowMenuController windowMenuController = new WindowMenuController();
+    Stage window = BattleshipsWindowed.getWindow();
+    WindowMenuController controller = new WindowMenuController();
 
 
     public void renderMainMenu(ArrayList<String> menuOptions) {
-        Stage window = BattleshipsWindowed.getWindow();
-
-        FXMLLoader fxmlLoader = new FXMLLoader(BattleshipsWindowed.class.getResource("menu.fxml"));
-        try {
-            this.menuScene = new Scene(fxmlLoader.load());
-        } catch (IOException e){
-            System.err.println("Could not load resource!");
-        }
+        FXMLLoader fxmlLoader = new FXMLLoader(BattleshipsWindowed.class.getResource("mainMenu.fxml"));
+        loadScene(fxmlLoader);
 
         VBox menuContainer = (VBox) menuScene.lookup("#menuContainer");
         menuContainer.setSpacing(50);
 
         int i = 1;
         for (String menuOption : menuOptions) {
-            Button button = new Button(menuOption.substring(3));
+            Button button = new Button(menuOption);
             button.setId(String.valueOf(i));
             button.setPrefWidth(400);
             button.setPrefHeight(50);
+            button.getStyleClass().add("dark-button");
+            button.setStyle("-fx-font-size: " + 18 + "px;");
+            button.setCursor(Cursor.HAND);
             menuContainer.getChildren().add(button);
-            button.setOnAction(e -> windowMenuController.onMainMenuBtnClick(button.getId()));
+            button.setOnAction(e -> controller.onMenuBtnClick(button.getId()));
             i++;
         }
         window.setScene(menuScene);
     }
 
     public void renderSettingsMenu(GameMode gameMode, ShootingMode shootingMode, int boardSize, int shootsPerPlayer){
+        FXMLLoader fxmlLoader = new FXMLLoader(BattleshipsWindowed.class.getResource("settingsMenu.fxml"));
+        loadScene(fxmlLoader);
+        WindowMenuController controller = fxmlLoader.getController();
+        controller.setGameModeBtn(gameMode);
+        controller.setShootModeBtn(shootingMode);
+        controller.setBoardSizeSlider(boardSize);
+        controller.setShotsSlider(shootsPerPlayer);
+        controller.addActionListeners();
+        window.setScene(menuScene);
+    }
 
+    private void loadScene(FXMLLoader fxmlLoader){
+        try {
+            this.menuScene = new Scene(fxmlLoader.load());
+        } catch (IOException e){
+            System.err.println("Could not load resource!");
+        }
     }
 }
